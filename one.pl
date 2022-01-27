608e6006998f271b1864af77054de64de99a5abf
aiur996 <70273781+aiur996@users.noreply.github.com>
2022-01-27 10:52:33 +0800

    added a file

---
+++ one.pl
@@ -0,0 +1,14 @@
+
+$data = "hello world";
+@ce = (2..6,f,f,gd);
+printf("1:@ce\n");
+push(@ce,edc);
+printf("2:@ce\n");
+pop(@ce);
+printf("3:@ce\n");
+shift(@ce);
+printf("4:@ce\n");
+unshift(@ce,99);
+printf("5:@ce\n");
+printf("6:$#ce\n");
+printf("7:$ce[-2]");
